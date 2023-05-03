16db0009d179cca4a6fab8b8e491ce5d74fafe64
SalipelaSwapna123 <130962712+SalipelaSwapna123@users.noreply.github.com>
2023-05-03 09:29:23 +0530

    Add files via upload

---
+++ Q1.sql
@@ -0,0 +1,10 @@
+-- Create database and its Name.
+-- create database rgm; 
+
+-- create table student(
+-- student_id int primary key not null,
+-- student_name varchar(35) not null,
+-- student_contact int not null,
+-- student_dept varchar(25) not null
+-- );
+select * from student;
